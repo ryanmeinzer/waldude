d867a7f9f91e5a19a86123d1564d197c75b3662b
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-07 18:44:54 -0400

    second commit

--- config/puma.rb
+++ config/puma.rb
@@ -16,6 +16,12 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
